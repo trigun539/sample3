bc217f1eabdcc5e59f3e19e8405f8bb6d4c9a56d
Edwin Perez <edwinperez539@gmail.com>
2015-04-11 14:37:54 -0400

    Aligning login items.

--- sample3/ViewController.swift
+++ sample3/ViewController.swift
@@ -8,6 +8,15 @@
 
 import UIKit
 
+func UIColorFromHex(rgbValue: UInt) -> UIColor {
+    return UIColor(
+        red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+        green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+        blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+        alpha: CGFloat(1.0)
+    )
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var theLabel: UILabel!
@@ -33,7 +42,7 @@ class ViewController: UIViewController {
         self.passwordField.text = "Password"
         self.loginBtn.setTitle("Login", forState: UIControlState.Normal)
         self.forgotPasswordBtn.setTitle("Forgot Password", forState: UIControlState.Normal)
-        self.view.backgroundColor = UIColor.yellowColor()
+        self.view.backgroundColor = UIColorFromHex(0xA1E8D9)
     }
 
     override func didReceiveMemoryWarning() {
